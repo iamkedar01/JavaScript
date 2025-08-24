fb079c9c94d2f2186ff580ecb968c6471a60066b
Kedar Dhage <kedardhage0@gmail.com>
2025-08-24 09:42:28 +0530

    Loops and String in JS

---
+++ Class_3/String.js
@@ -0,0 +1,70 @@
+// String in Js 
+// String is sequence of the character 
+
+// let str1 = "Java Script ";
+// let str2 = "Apana college";
+// console.log("str1 is :",str1);
+// console.log("Str2  is :",str2);
+// // This will give the length of the string using the length
+// console.log("str1 length is :",str1.length);
+// console.log("Str2 length is :",str2.length);
+//  // we can accees the length of the given string using the indices
+//  // Here  aware of the space in a given in string bcz indices that also consider the in length 
+// console.log("str1 char 1 is :",str1[1]);
+// console.log("Str2 char 1 is :",str2[1]);
+
+// // Template Literals in js
+// let specialString =`his is a  template literal `; 
+// //this string is written using the baclikt which present in below of esc button 
+// console.log(specialString);
+// console.log(typeof specialString);
+
+// // using the object acces the element
+// let obj = {
+//     item : "pen",
+//     price : 10,
+//     color : "Black"
+// }
+// console.log("The ",obj.color,"color", obj.item," price is : ",obj.price,"rupees"); 
+// // here numbers will be highlighted in output 
+// // The Template Literal is similary to the fstring in python help to print the output
+// console.log(`The ${obj.color}color ${obj.item} price is: ${obj.price} rupees`);
+// output = `The ${obj.color}color ${obj.item} price is: ${obj.price} rupees`;
+// console.log(output)
+
+
+// note
+//  here must maintain the ${} this pattern to print the variable in js inside the baclikt
+ // it is also called string interpolation 
+ // in this template we can add the opration  also add like a+b a-b etc
+ // escape sequenc like 
+ // \n --> it is used to enter the new line 
+ // \t --> it is udes to enter tab space between the words 
+//  console.log("java \nScript"); // 
+//  let str = "java\tScript"
+
+// console.log(`the length of ${str} is ${str.length} `);
+//  //  here the length of string became 11 \t both consider has single character 
+
+
+// String methods in js 
+// In js string are the immutabe  means you can chnage or madify once you declare the string
+let str = "  Java script  ";
+console.log(str.trim()); // it is used to remove the space in the given string 
+console.log(str.toUpperCase()); // it is used to convert the upper case 
+console.log(str.toLowerCase()); //  it is used to convert the lower case
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Class_3/script.js
@@ -0,0 +1,76 @@
+// Loopes are used to execute the pecipe of code agian and agian 
+// Detail explination is given below with example
+
+// console.log("Hello World..!");
+// console.log("Hello World..!");
+// console.log("Hello World..!");
+// console.log("Hello World..!");
+// console.log("Hello World..!");
+
+// this line of cod ecan be reduce useing the loopes 
+
+// for (let i = 0; i<10;i++){
+//     console.log("hello world...!");
+// }
+// console.log("loop is ended");
+
+// // calculate the sum of the given number 
+// let num = prompt("Enter a number:");
+// let sum =0;
+// for (let i = 0 ;i<= num;i++){ // here must aware of the logic and condition
+//     sum= sum+i;
+//     // sum +=i
+// }
+// console.log("sum of ",num,"is:",sum);
+
+
+// // here checking how for loop  works
+// for (let i =0; i<=5;i++) {
+//     console.log("i =",i);
+// }
+
+// infinite loop means never ending loop 
+
+// while loop in js 
+// //here checking how for loop  works
+// let i=0;
+// while(i<= 5){
+//     console.log("i =",i);
+//     i++
+// }
+
+// do while loop in js
+// //here checking how for loop  works
+// let i=0;
+// do{
+//     console.log("i =",i);
+//     i++;  // In the do while loop must aware of the increment option otherwise it will run infinite loop 
+// }while(i<=5);
+
+// // // for of loop in js in details 
+// // // for of loop is used to iterate thorught the string and arrays
+// let str = "Welcome to the Js";
+// let size =0;
+// for (let i of str){
+//     console.log("i= ",i); // give the every character in a string 
+//     size++;
+    
+// }
+// console.log("size of the string is :", size);
+
+
+// // for in loop in js in details 
+// // it  is used for the objects iterations in js
+// let student = { // object creation in java script
+//     name : "student", 
+//     age : 27,
+//     cgps : 8.8,
+//     ispass : true
+// }
+// for(let key in student ){
+//     console.log("key=",key ,"value=",student[key]);
+// }
+
+
+
+
