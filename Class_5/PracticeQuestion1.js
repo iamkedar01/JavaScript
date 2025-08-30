0392529080f4effce28e165bcb26b2c49f585fdf
Kedar Dhage <kedardhage0@gmail.com>
2025-08-30 21:33:00 +0530

    Practice Questions on Functions

--- Class_5/PracticeQuestion1.js
+++ Class_5/PracticeQuestion1.js
@@ -10,12 +10,26 @@
 */
 
 // The diffrent way of write the call Back function
-let nums = [1,2,3,4,5,6,7,8,9,10];
-let CalcSqure = (num)=>{
-    console.log(`The Squre of the ${num} is: ${num**2}`);// num *num
-}
-nums.forEach(CalcSqure);
+// let nums = [1,2,3,4,5,6,7,8,9,10];
+// let CalcSqure = (num)=>{
+//     console.log(`The Squre of the ${num} is: ${num**2}`);// num *num
+// }
+// nums.forEach(CalcSqure);
 /*  In this above vode using the function delcare the call back functions
     after that pass that function name as paramter to the forEach loop.
 
 */
+
+// find the how many vowels in the given string 
+
+let str ="The javascripe is good Programming language";
+let count=0;
+function Vowelscount(str){
+    for (var char of str){
+        if (char=="a"|| char=="e" || char=="i"||char=="o"||char=="u"||char=="A"|| char=="E" || char=="I"||char=="O"||char=="U"){
+        count+=1;
+        }
+    }
+console.log(`The number of voules in the is : ${count}`);
+}
+Vowelscount(str);
