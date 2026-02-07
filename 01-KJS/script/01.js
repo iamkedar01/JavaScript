442e283b50136865c1ea40495b15829c3b50f097
Kedar Dhage <kedardhage0@gmail.com>
2026-02-07 09:57:21 +0530

    01 - Java Script in Kannada

---
+++ 01-KJS/script/01.js
@@ -0,0 +1,10 @@
+
+ // this the first line of the code 
+ // this the single line code
+console.log("Hello world")
+
+/*
+ this the multiple line code comments 
+ here we can write the multi lines commemnds 
+*/
+ 
